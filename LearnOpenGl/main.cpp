24d3c39aaf675d78726d6b74189896c4efee571a
qiaogaojian <qiaogaojian@vip.qq.com>
2020-11-27 16:52:38 +0800

    使用glad代替glew

--- LearnOpenGl/main.cpp
+++ LearnOpenGl/main.cpp
@@ -1,40 +1,41 @@
-#include<iostream>
-#include <GL/glew.h>
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-#define GLEW_STATIC
+#include <iostream>
 
-using namespace std;
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
 
-int main(int argc, char** argv[])
+int main()
 {
-	/*glewExperimental = GL_TRUE;
-	if (glewInit()!=GLEW_OK)
-	{
-	cout << "failed to initalize GLEW" << endl;
-	return -1;
-	}*/
-
-	glfwInit();                                                     //³õÊ¼»¯
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);                  //ÅäÖÃGLFW
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);                  //ÅäÖÃGLFW
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
-	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", nullptr, nullptr);
-
-	if (window == nullptr)
+	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+	if (window == NULL)
 	{
-		cout << "Failed to create GLFW window" << endl;
+		std::cout << "Failed to create GLFW window" << std::endl;
 		glfwTerminate();
 		return -1;
 	}
-
 	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
 	while (!glfwWindowShouldClose(window))
 	{
-		glfwPollEvents();
+
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
 		glfwSwapBuffers(window);
+		glfwPollEvents();
 	}
 
 	glfwTerminate();
