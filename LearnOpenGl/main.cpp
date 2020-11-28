97fce0a1cc90d78462760f2a133cd2c246244810
qiaogaojian <qiaogaojian@vip.qq.com>
2020-11-28 11:11:02 +0800

    重构三角形顶点变量数组名字

--- LearnOpenGl/main.cpp
+++ LearnOpenGl/main.cpp
@@ -22,11 +22,11 @@ const char* fragmentShaderSource =
 "{\n"
 "	FragColor = vec4(1.0f,0.5f,0.2f,1.0f);\n"
 "}\n\0";
-
-float vertices[] = {
-	-0.5f,-0.5f,0.0f,
-	0.5f,-0.5f,0.0f,
-	0.0f,0.5f,0.0f
+// Èý½ÇÐÎ¶¥µã
+float triVertices[] = {
+	-0.5f, -0.5f, 0.0f,
+	 0.5f, -0.5f, 0.0f,
+	 0.0f,  0.5f, 0.0f
 };
 
 void onStart();
@@ -121,7 +121,7 @@ int main()
 	glBindVertexArray(VAO);
 
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(triVertices), triVertices, GL_STATIC_DRAW);
 
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
