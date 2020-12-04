eabf25e8bfd3caa0f3dc70e0f32a2bc3bb8d9d06
qiaogaojian <qiaogaojian@vip.qq.com>
2020-12-04 18:12:02 +0800

    重构代码

---
+++ src/Utils/shader_loader.h
@@ -0,0 +1,41 @@
+#ifndef SHADER_H
+#define SHADER_H
+
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+using namespace std;
+using namespace glm;
+
+class ShaderLoader
+{
+public:
+	unsigned int ID;
+	ShaderLoader(const char* vertexPath, const char* fragmentPath, const char* geometryPath = nullptr);
+
+	// 激活shader
+	void use();
+
+	// uniform工具函数
+	void setBool(const string& name, bool value) const;
+	void setInt(const string& name, int value) const;
+	void setFloat(const string& name, float value) const;
+	void setVec2(const string& name, const vec2& value) const;
+	void setVec2(const string& name, float x, float y) const;
+	void setVec3(const string& name, const vec3& value) const;
+	void setVec3(const string& name, float x, float y, float z) const;
+	void setVec4(const string& name, const vec4& value) const;
+	void setVec4(const string& name, float x, float y, float z, float w) const;
+	void setMat2(const string& name, const mat2& mat) const;
+	void setMat3(const string& name, const mat3& mat) const;
+	void setMat4(const string& name, const mat4& mat) const;
+
+private:
+	// 检查shader 编译 链接错误
+	void checkCompileErrors(GLuint shader, string type);
+};
+#endif
