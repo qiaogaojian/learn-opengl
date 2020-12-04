8129a6584be051e60f50652690365f07b7f2e7c7
qiaogaojian <qiaogaojian@vip.qq.com>
2020-12-04 11:17:40 +0800

    给窗口添加返回键退出功能

--- src/1.0 Hello Window/hello_window.cpp
+++ src/1.0 Hello Window/hello_window.cpp
@@ -42,6 +42,8 @@ int main()
 
     while (!glfwWindowShouldClose(window))
     {
+        processInput(window);
+
         // 检查并调用事件，交换缓冲
 		glfwPollEvents();			// 检查有没有触发什么事件（比如键盘输入、鼠标移动等）、更新窗口状态，并调用对应的回调函数（可以通过回调方法手动设置）
 		glfwSwapBuffers(window);	// 双缓冲交换颜色缓冲（它是一个储存着GLFW窗口每一个像素颜色值的大缓冲），它在这一迭代中被用来绘制，并且将会作为输出显示在屏幕上
