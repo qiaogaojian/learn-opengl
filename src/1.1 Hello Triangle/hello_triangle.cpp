703c0d05e7bbe31fbba6b07c33f2137c34426e55
qiaogaojian <qiaogaojian@vip.qq.com>
2020-12-04 11:21:44 +0800

    三角形练习 复用window创建代码

---
+++ src/1.1 Hello Triangle/hello_triangle.cpp
@@ -0,0 +1,74 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+using namespace std;
+
+const unsigned int SCR_WIDTH = 800;  // 屏幕宽度
+const unsigned int SCR_HEIGHT = 600; // 屏幕高度
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+void processInput(GLFWwindow *window);
+
+int main()
+{
+    // 初始化和配置 glfw
+    //--------------------------------------------------------------------------------------
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);  // 如果使用的是Mac OS X系统 需要解注释这行代码
+
+    // 创建窗口对象
+    GLFWwindow *window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Learn OpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    // 初始化 glad  加载所有 opengl 的函数指针
+    //--------------------------------------------------------------------------------------
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    while (!glfwWindowShouldClose(window))
+    {
+        // 处理输入
+        processInput(window);
+
+        //处理渲染
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f); // 设置状态
+        glClear(GL_COLOR_BUFFER_BIT);         // 使用状态
+
+        // 检查并调用事件，交换缓冲完成绘制
+        glfwPollEvents();        // 检查有没有触发什么事件（比如键盘输入、鼠标移动等）、更新窗口状态，并调用对应的回调函数（可以通过回调方法手动设置）
+        glfwSwapBuffers(window); // 双缓冲交换颜色缓冲（它是一个储存着GLFW窗口每一个像素颜色值的大缓冲），它在这一迭代中被用来绘制，并且将会作为输出显示在屏幕上
+    }
+
+    glfwTerminate();
+    return 0;
+}
+
+// 窗口大小改变时 调整视口大小
+void framebuffer_size_callback(GLFWwindow *window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+// 处理输入
+void processInput(GLFWwindow *window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
