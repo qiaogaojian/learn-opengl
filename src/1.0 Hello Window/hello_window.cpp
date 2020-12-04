306539c7759ba82edd2e3288c2e7f01aff65a3ce
qiaogaojian <qiaogaojian@vip.qq.com>
2020-12-04 11:20:11 +0800

    添加背景颜色 完成窗口的创建

--- src/1.0 Hello Window/hello_window.cpp
+++ src/1.0 Hello Window/hello_window.cpp
@@ -42,11 +42,16 @@ int main()
 
     while (!glfwWindowShouldClose(window))
     {
+        // 处理输入
         processInput(window);
 
-        // 检查并调用事件，交换缓冲
-		glfwPollEvents();			// 检查有没有触发什么事件（比如键盘输入、鼠标移动等）、更新窗口状态，并调用对应的回调函数（可以通过回调方法手动设置）
-		glfwSwapBuffers(window);	// 双缓冲交换颜色缓冲（它是一个储存着GLFW窗口每一个像素颜色值的大缓冲），它在这一迭代中被用来绘制，并且将会作为输出显示在屏幕上
+        //处理渲染
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f); // 设置状态
+        glClear(GL_COLOR_BUFFER_BIT);         // 使用状态
+
+        // 检查并调用事件，交换缓冲完成绘制
+        glfwPollEvents();        // 检查有没有触发什么事件（比如键盘输入、鼠标移动等）、更新窗口状态，并调用对应的回调函数（可以通过回调方法手动设置）
+        glfwSwapBuffers(window); // 双缓冲交换颜色缓冲（它是一个储存着GLFW窗口每一个像素颜色值的大缓冲），它在这一迭代中被用来绘制，并且将会作为输出显示在屏幕上
     }
 
     glfwTerminate();
