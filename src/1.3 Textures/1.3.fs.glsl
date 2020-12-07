7ba0517652c36b77d15371f4dcd4a1f121924d6b
qiaogaojian <qiaogaojian@vip.qq.com>
2020-12-07 20:15:14 +0800

    修改片元着色器 混合颜色

--- src/1.3 Textures/1.3.fs.glsl
+++ src/1.3 Textures/1.3.fs.glsl
@@ -10,5 +10,5 @@ uniform sampler2D texture1;
 void main()
 {
 	// FragColor = vec4(1.0f,0.5f,0.2f,1.0f);
-	FragColor = texture(texture1, TexCoord);
+	FragColor = texture(texture1, TexCoord) * vec4(ourColor,1.0);
 }
