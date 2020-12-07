fa333812bf83860a070689a9885c68a93cbefb59
qiaogaojian <qiaogaojian@vip.qq.com>
2020-12-07 19:00:01 +0800

    texture shader

--- src/1.3 Textures/1.3.fs.glsl
+++ src/1.3 Textures/1.3.fs.glsl
@@ -1,12 +1,14 @@
 #version 330 core
+out vec4 FragColor;
 
 in vec3 ourColor;
 in vec2 TexCoord;
-out vec4 FragColor;
 
-uniform sampler2D ourTexture;
+// texture sampler
+uniform sampler2D texture1;
 
 void main()
 {
-    FragColor=texture(ourTexture,TexCoord);
+	// FragColor = vec4(1.0f,0.5f,0.2f,1.0f);
+	FragColor = texture(texture1, TexCoord);
 }
