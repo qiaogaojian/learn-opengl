8943c20ffb4f35181d1ad4fb6bb84c0b51785953
qiaogaojian <qiaogaojian@vip.qq.com>
2020-12-12 11:09:20 +0800

    摄像机 练习1 fps相机 移动坐标时保持y轴不变

--- src/1.6 Camera/practice1.h
+++ src/1.6 Camera/practice1.h
@@ -82,6 +82,7 @@ public:
             Position += Right * velocity;
             break;
         }
+        Position.y = 0.0f;
     }
 
     void ProcessMouseMovement(float xoffset, float yoffset, GLboolean constrainPitch = true)
