0a7f14a60703f842eee17149f64ad3b4c8018342
qiaogaojian <qiaogaojian@vip.qq.com>
2020-12-17 14:55:24 +0800

    复用代码 basic_lighting.cpp

---
+++ src/2.3 Materials/materials.fs
@@ -0,0 +1,33 @@
+#version 330 core
+in vec3 Normal;
+in vec3 FragPos;
+
+out vec4 FragColor;
+
+uniform vec3 objectColor;
+uniform vec3 lightColor;
+uniform vec3 lightPos;
+uniform vec3 viewPos;
+
+void main()
+{
+    // 环境光
+    float ambientStrength=.1;
+    vec3 ambient=ambientStrength*lightColor;
+
+    // 漫反射
+    vec3 normalDir=normalize(Normal);
+    vec3 lightDir=normalize(lightPos-FragPos);
+    float diff=max(dot(normalDir,lightDir),0);
+    vec3 diffuse=diff*lightColor;
+
+    // 镜面反射
+    float specularStrength=.5;
+    vec3 viewDir=normalize(viewPos-FragPos);
+    vec3 reflectDir=reflect(-lightDir,normalDir);// 反射函数第一个参数是入射光方向 第二个参数是法线方向
+    float spec=pow(max(dot(viewDir,reflectDir),0),32);
+    vec3 specular=specularStrength*spec*lightColor;
+
+    vec3 result=(ambient+diffuse+specular)*objectColor;
+    FragColor=vec4(result,1.);
+}
