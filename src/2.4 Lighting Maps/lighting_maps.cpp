ee916f36a5a7df02ffe4b9a33a8f2ca27302adb1
qiaogaojian <qiaogaojian@vip.qq.com>
2020-12-18 11:54:52 +0800

    remove comment

--- src/2.4 Lighting Maps/lighting_maps.cpp
+++ src/2.4 Lighting Maps/lighting_maps.cpp
@@ -156,31 +156,6 @@ int main()
     texPath = shaderObject.concatString(getcwd(NULL, 0), "/res/texture/box2_specular.png");
     unsigned int texture_specular = loadTexture(texPath.c_str());
 
-    // unsigned int texture;
-    // glGenTextures(1, &texture);
-    // glBindTexture(GL_TEXTURE_2D, texture);
-    // // 为当前绑定的纹理对象设置环绕 过滤方式
-    // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    // // 加载并生成纹理
-    // int width;
-    // int height;
-    // int nrChannels;
-    // stbi_set_flip_vertically_on_load(true); // OpenGL要求y轴0.0坐标是在图片的底部的，但是图片的y轴0.0坐标通常在顶部,这里进行上下翻转
-    // string texPath = shaderObject.concatString(getcwd(NULL, 0), "/res/texture/box2.png");
-    // unsigned char *data = stbi_load(texPath.c_str(), &width, &height, &nrChannels, 0);
-    // if (data)
-    // {
-    //     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
-    //     glGenerateMipmap(GL_TEXTURE_2D);
-    // }
-    // else
-    // {
-    //     cout << "材质加载失败" << endl;
-    // }
-    // stbi_image_free(data);
     shaderObject.use();
     shaderObject.setInt("material.diffuse",0);
     shaderObject.setInt("material.specular",1);
