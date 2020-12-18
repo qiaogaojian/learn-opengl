40d8d3dca09d0acb5a536173704e9a64f3890804
qiaogaojian <qiaogaojian@vip.qq.com>
2020-12-18 13:55:24 +0800

    反转镜面光照贴图测试

--- src/2.4 Lighting Maps/practice2.fs
+++ src/2.4 Lighting Maps/practice2.fs
@@ -38,7 +38,7 @@ void main()
     vec3 viewDir=normalize(viewPos-FragPos);
     vec3 reflectDir=reflect(-lightDir,normalDir);// 反射函数第一个参数是入射光方向 第二个参数是法线方向
     float spec=pow(max(dot(viewDir,reflectDir),0),32);
-    vec3 specular=light.specular * spec * vec3(texture(material.specular,TexCoord));
+    vec3 specular=light.specular * spec *(vec3(1.0) - vec3(texture(material.specular,TexCoord)));
 
     vec3 result=ambient+diffuse+specular;
     FragColor=vec4(result,1.);
