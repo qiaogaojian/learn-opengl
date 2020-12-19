e48161ba2c2bd1a8ac55a640eedee8af2e46e962
qiaogaojian <qiaogaojian@vip.qq.com>
2020-12-19 16:57:54 +0800

    抽象网格类

---
+++ src/3.2 Mesh/mesh.h
@@ -0,0 +1,120 @@
+#ifndef MESH_H
+#define MESH_H
+
+#include <glad.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#include "shader_loader.h"
+
+#include <string>
+#include <vector>
+using namespace std;
+using namespace glm;
+
+struct Vertex
+{
+    vec3 Position;
+    vec3 Normal;
+    vec2 TexCoords;
+    vec3 Tangent;
+    vec3 Bitangent;
+};
+
+struct Texture
+{
+    unsigned int id;
+    string type;
+    string path;
+};
+
+class Mesh
+{
+public:
+    vector<Vertex> vertices;
+    vector<unsigned int> indices;
+    vector<Texture> textures;
+
+    unsigned int VAO;
+
+    Mesh(vector<Vertex> vertices, vector<unsigned int> indices, vector<Texture> textures)
+    {
+        this->vertices = vertices;
+        this->indices = indices;
+        this->textures = textures;
+
+        setupMesh();
+    }
+
+    void Draw(ShaderLoader shader)
+    {
+        unsigned int diffuseNumber = 1;
+        unsigned int specularNumber = 1;
+        unsigned int normalNumber = 1;
+        unsigned int heightNumber = 1;
+
+        for (unsigned int i = 0; i < textures.size(); i++)
+        {
+            glActiveTexture(GL_TEXTURE0 + i);
+
+            string number;
+            string name = textures[i].type;
+
+            if(name == "texture_diffuse")
+                number = to_string(diffuseNumber);
+            if(name == "texture_specular")
+                number = to_string(specularNumber);
+            if(name == "texture_normal")
+                number = to_string(normalNumber);
+            if(name == "texture_height")
+                number = to_string(heightNumber);
+
+            // shader.setInt((name + number).c_str(), i);
+            glUniform1i(glGetUniformLocation(shader.ID, (name + number).c_str()), i);  // 参考 lighting_maps.cpp 160 行
+        }
+
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
+
+        glActiveTexture(GL_TEXTURE0);   // 设置完成后恢复默认
+    }
+
+private:
+    // 缓冲数据
+    unsigned int VBO, EBO;
+    // 初始化
+    void setupMesh()
+    {
+        glGenVertexArrays(1, &VAO);
+        glGenBuffers(1, &VBO);
+        glGenBuffers(1, &EBO);
+
+        glBindVertexArray(VAO);
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(Vertex), &vertices[0], GL_STATIC_DRAW);
+
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), &indices[0], GL_STATIC_DRAW);
+
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void *)0);
+
+        glEnableVertexAttribArray(1);
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void *)offsetof(Vertex, Normal));
+
+        glEnableVertexAttribArray(2);
+        glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void *)offsetof(Vertex, TexCoords));
+
+        glEnableVertexAttribArray(3);
+        glVertexAttribPointer(3, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void *)offsetof(Vertex, Tangent));
+
+        glEnableVertexAttribArray(4);
+        glVertexAttribPointer(4, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void *)offsetof(Vertex, Bitangent));
+
+        glBindVertexArray(0);
+    }
+};
+
+#endif
